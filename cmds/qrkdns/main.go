577371f2e230c4d73a98370a54545d599c7137a5
Mark Liederbach <10621536+markliederbach@users.noreply.github.com>
2021-10-16 10:23:37 -0500

    change main app

--- cmds/qrkdns/main.go
+++ cmds/qrkdns/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"time"
 
 	"github.com/markliederbach/qrkdns/pkg/controllers"
 	log "github.com/sirupsen/logrus"
@@ -24,38 +23,8 @@ const (
 	LogLevelFlag string = "log-level"
 )
 
-// NewApp creates a new CLI app
-func NewApp(version string, commands []*cli.Command) *cli.App {
-	return &cli.App{
-		Name:     "qrkdns",
-		Version:  version,
-		Usage:    "Automatically update Cloudflare DNS records",
-		Compiled: time.Now(),
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    LogLevelFlag,
-				Aliases: []string{"l"},
-				Value:   "INFO",
-				Usage:   "Set the log output",
-				EnvVars: []string{"LOG_LEVEL"},
-			},
-		},
-		Before: func(c *cli.Context) error {
-			log.SetFormatter(&log.JSONFormatter{})
-			logrusLevel, err := log.ParseLevel(c.String(LogLevelFlag))
-			if err != nil {
-				return err
-			}
-			log.SetLevel(logrusLevel)
-			log.WithField("version", Version).Debug("Running qrkdns")
-			return nil
-		},
-		Commands: commands,
-	}
-}
-
 func main() {
-	app := NewApp(Version, Commands)
+	app := controllers.NewQrkDNSApp(Version, Commands)
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Error(err)
